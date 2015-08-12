de0b2fd70ca70561fb51aea09656e44def0b8ab1
mkoertgen <marcel.koertgen@gmail.com>
2015-08-12 21:53:44 +0200

    .

--- helloNeo4j/Neo4JLearningTests.cs
+++ helloNeo4j/Neo4JLearningTests.cs
@@ -1,13 +1,16 @@
 ﻿using System;
-using System.Linq;
+using System.Collections.Generic;
+using System.IO;
 using FluentAssertions;
 using Neo4jClient;
+using Neo4jClient.Cypher;
 using NUnit.Framework;
 
 namespace helloNeo4j
 {
-    [TestFixture]
-    class Neo4JLearningTests
+    [TestFixture(Description = "Movie Graph Intro, cf.: http://neo4j.com/docs/stable/cypherdoc-movie-database.html")]
+    [Ignore, Explicit]
+    class Neo4JLearningTests : IDisposable
     {
         private static readonly IGraphClient Client = GetGraphClient();
 
@@ -17,39 +20,23 @@ namespace helloNeo4j
             Client.ServerVersion.Should().BeGreaterOrEqualTo(new Version(2, 2));
         }
 
-        public class Book
-        {
-            public string Title { get; set; }
-            public int Pages { get; set; }
-        }
-
-        public class User
-        {
-            public long Id { get; set; }
-            public string Name { get; set; }
-            public int Age { get; set; }
-            public string Email { get; set; }
-        }
-
-        [Test(Description = "Get specific user, cf.: https://github.com/Readify/Neo4jClient/wiki/cypher-examples#get-specific-user")]
-        public void TestSimpleQuery()
-        {
-            var jim = Client.Cypher
-                .Match("(user:User)")
-                .Where((User user) => user.Id == 456)
-                .Return(user => user.As<User>())
-                .Results.Single();
-            jim.Id.Should().Be(456);
-        }
-
         static IGraphClient GetGraphClient(string endpoint = "http://neo4j:admin@localhost:7474/db/data")
         {
             // https://github.com/Readify/Neo4jClient/wiki/connecting
             var client = new GraphClient(new Uri(endpoint));
             client.Connect();
 
+            //SetupTestData(client);
+
+
+            return client;
+        }
+
+        private static void SetupTestData(ICypherGraphClient client)
+        {
+            /*
             // https://github.com/Readify/Neo4jClient/wiki/cypher-examples#create-a-user-only-if-they-dont-already-exist
-            var newUser = new User { Id = 456, Name = "Jim" };
+            var newUser = new User {Id = 456, Name = "Jim"};
             client.Cypher
                 .Merge("(user:User { Id: {id} })")
                 .OnCreate()
@@ -60,8 +47,30 @@ namespace helloNeo4j
                     newUser
                 })
                 .ExecuteWithoutResults();
+            */
 
-            return client;
+
+            // movie data base
+            var queryText = File.ReadAllText("CreateMovieDb.cypher");
+            ExecuteCypher(client, queryText);
+        }
+
+        private static void ExecuteCypher(ICypherGraphClient client, string queryText)
+        {
+            // https://github.com/Readify/Neo4jClient/wiki/cypher#manual-queries-highly-discouraged
+            var query = new CypherQuery(queryText, new Dictionary<string, object>(), CypherResultMode.Projection);
+            ((IRawGraphClient)client).ExecuteCypher(query);
+        }
+
+        [TestFixtureTearDown]
+        public void Dispose()
+        {
+            //MATCH(a: Person),(m:Movie) OPTIONAL MATCH (a) -[r1] - (), (m) -[r2] - () DELETE a, r1, m, r2
+            var query = Client.Cypher
+                .Match("(a: Person),(m:Movie)")
+                .OptionalMatch("(a) -[r1] - (), (m) -[r2] - ()")
+                .Delete("a, r1, m, r2");
+            query.ExecuteWithoutResults();
         }
     }
 }
